70e4decaaf800f2bf8c93a67cb228be89a1c8928
Pro Team <39613333+psxpage@users.noreply.github.com>
2022-07-06 09:23:55 +0800

    Update 900.js

--- indo900/900.js
+++ indo900/900.js
@@ -696,7 +696,7 @@ function kernelExploit() {
    }
   }
   chain.run(); 
-  alert("\n\n------------------------------Masukkan Flashdisk Sekarang.------------------------------\n-----------Klik OK setelah muncul notifikasi 'USB unsupported/USB tidak didukung'.-----------");
+  alert("\n\n---------------------------Masukkan Flashdisk Sekarang---------------------------\n--------Klik OK setelah muncul notifikasi 'USB unsupported/USB tidak didukung'--------");
   {
    for (var i = 1; i < NUM_KQUEUES; i += 2) {
     chain.fcall(window.syscalls[6], kqueues[i]);
@@ -711,7 +711,7 @@ function kernelExploit() {
    chain.run();
    return;
   }
-  alert("Kernel Exploit Failed! Reboot and Try Again.");
+  alert("Kernel Exploit Gagal! Reboot dan Coba lagi.");
   p.write8(0, 0);
   return;
  };
